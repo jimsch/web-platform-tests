1ea5a29eb0d6f360090f93cb907ad3182cfe9694
Jim Schaad <ietf@augustcellars.com>
2016-03-21 18:39:11 -0700

    Fix my typos to get it to work correctly

--- WebCryptoAPI/HKDF_derivebits_known.js
+++ WebCryptoAPI/HKDF_derivebits_known.js
@@ -21,7 +21,7 @@ var Knowns = [
     },
     {
         name: "Test with SHA-256 and zero-length salt/info",
-        Hash: "SHA-256",
+        hash: "SHA-256",
         IKM: "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b", // (22 octets)
         salt: "", // (0 octets)
         info: "", // (0 octets)
@@ -31,7 +31,7 @@ var Knowns = [
     },
     { 
         name: "Basic test case with SHA-1",
-        Hash: "SHA-1",
+        hash: "SHA-1",
         IKM: "0b0b0b0b0b0b0b0b0b0b0b", // (11 octets)
         salt: "000102030405060708090a0b0c", // (13 octets)
         info: "f0f1f2f3f4f5f6f7f8f9", //  (10 octets)
@@ -41,7 +41,7 @@ var Knowns = [
     },
     {
         name: "Test with SHA-1 and longer inputs/outputs",
-        Hash: "SHA-1",
+        hash: "SHA-1",
         IKM: "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f", // (80 octets)
         salt: "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeaf", // (80 octets)
         info: "b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff", // (80 octets)
@@ -51,7 +51,7 @@ var Knowns = [
     },
     {
         name: "Test with SHA-1 and zero-length salt/info",
-        Hash: "SHA-1",
+        hash: "SHA-1",
         IKM: "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b", //  (22 octets)
         salt: "", // (0 octets)
         info: "", // (0 octets)
@@ -61,7 +61,7 @@ var Knowns = [
     },
     {
         name: "Test with SHA-1, salt not provided (defaults to HashLen zero octets), zero-length info",
-        Hash: "SHA-1",
+        hash: "SHA-1",
         IKM: "0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c", //  (22 octets)
         salt: null, // not provided (defaults to HashLen zero octets)
         info: "", // (0 octets)
